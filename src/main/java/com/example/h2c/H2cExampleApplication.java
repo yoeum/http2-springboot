9c58b8607d0bb8c2c79252e80fc43e0635961ea8
yoeum <42525190+yoeum@users.noreply.github.com>
2024-08-29 15:18:04 +0900

    Create H2cExampleApplication.java

---
+++ src/main/java/com/example/h2c/H2cExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.h2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2cExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2cExampleApplication.class, args);
+    }
+}
